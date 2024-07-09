d5e00fc9d6e22dd9cb456cc7d0821dfc45e3662c
nikolaipaukkonen <nikolai.paukkonen@helsinki.fi>
2024-07-09 15:55:33 +0300

    --

--- src/app/page.js
+++ src/app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
                 const base64Audio = reader.result.split(',')[1]; // Remove the data URL prefix
 
                 const response = await fetch("/api/speechToText", {
-                  Allow: GET, POST, HEAD,
+                  Allow: POST,
                   method: "POST",
                   headers: {
                     'Content-Type': 'application/json',
